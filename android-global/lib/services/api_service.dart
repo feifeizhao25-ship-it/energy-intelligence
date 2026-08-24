ea3401044753d0a6afd759b5fcb9cef156e69e74
feifei00 <feifei00@feifei00deMacBook-Air-6.local>
2026-08-24 15:10:42 +0800

    chore: clean mobile analyzer warnings

--- android-global/lib/services/api_service.dart
+++ android-global/lib/services/api_service.dart
@@ -4,7 +4,6 @@
 library;
 
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
